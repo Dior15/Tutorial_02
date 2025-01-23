6f61df0e6efe077d7ab72b5139caf0e0d8301af5
Khristian Diorico <77899722+Dior15@users.noreply.github.com>
2025-01-23 12:06:24 -0500

    Finished

--- Tutorial_02_starter_code.py
+++ Tutorial_02_starter_code.py
@@ -1,32 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from ... import *
-from ... import *
+from bisection import *
+from newton import *
 
 # The function for the unknown interest rate problem. x is the percentual interest rate and parameters are set as in lecture 2:
 def f(x):
-    return 
+    return 1-(1/((1+x)**12))-10*x
 
 def df(x):
-    return 
+    return (12/((1+x)**13))-10
 
 # First try bisection:
-l=
-r= 
+l=0.02
+r= 0.04
 kMax = 50
-eps_x = 
-eps_f = 
+eps_x = 1.0e-10
+eps_f = 1.0e-10
 
- ... = bis( ... )
+bis_iterations, bis_conv = bis(f,l,r,eps_x,eps_f,kMax)
+
+print('_____________')
 
 # Now use newton:
-x0=
-kMax = 
- ... = Newton( ... )
+x0=0.04
+kMax = 20
+newton_iterations, newton_conv = Newton(f,df,x0,eps_x,eps_f,kMax)
+
+bis_error = [inside[1] for inside in bis_iterations]
+newton_error = [inside[1] for inside in newton_iterations]
 
 # Now plot on a semilof (y) scale to see a straight line for bisection:
-plt.semilogy( ... ,'-*r')
-plt.semilogy( ... ,'-*k')
+plt.semilogy(bis_error,'-*r')
+plt.semilogy(newton_error,'-*k')
 plt.title('Red: bisection. Black: Newton iteration.')
 plt.xlabel('Nr. of iterations')
 plt.ylabel('error')
